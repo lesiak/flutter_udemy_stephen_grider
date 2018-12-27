e33d1963aefea820a7dbe1bc28c0f013ac7ae8dd
lesiak <lesiak@outlook.com>
2018-12-27 12:13:16 +0100

    [LOGIN_BLOC] Input widgets

--- app_2_login_bloc/lib/src/screens/login_screen.dart
+++ app_2_login_bloc/lib/src/screens/login_screen.dart
@@ -7,9 +7,41 @@ class LoginScreen extends StatelessWidget {
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: false,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
     );
   }
 }

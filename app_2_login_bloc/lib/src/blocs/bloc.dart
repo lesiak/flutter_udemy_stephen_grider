92e2038e3439581c0250280ca7764cf9f1b7fcfb
lesiak <lesiak@outlook.com>
2018-12-31 20:34:31 +0100

    [LOGIN_BLOC] Added bloc for login widget

---
+++ app_2_login_bloc/lib/src/blocs/bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'validators.dart';
+
+class Bloc extends Object with Validators {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  // Add data to stream
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+
+  Stream<String> get password => _passwordController.stream.transform(validatePassword);
+
+  // Change data
+  Function(String) get changeEmail => _emailController.sink.add;
+
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  void dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+}
+
+final bloc = Bloc();

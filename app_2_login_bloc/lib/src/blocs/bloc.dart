39e31570f07ac7029a0ce9146358fdfcd0454a9f
lesiak <lesiak@outlook.com>
2019-01-07 13:32:10 +0100

    [LOGIN_BLOC] Combined streams to disable Submit button

--- app_2_login_bloc/lib/src/blocs/bloc.dart
+++ app_2_login_bloc/lib/src/blocs/bloc.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
 import 'validators.dart';
 
 class Bloc extends Object with Validators {
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
 
   // Add data to stream
   Stream<String> get email => _emailController.stream.transform(validateEmail);
 
   Stream<String> get password => _passwordController.stream.transform(validatePassword);
+
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
 
   // Change data
   Function(String) get changeEmail => _emailController.sink.add;

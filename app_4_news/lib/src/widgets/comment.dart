c9191562b2f0778dbf21cde880392bb8f2e01a96
lesiak <lesiak@outlook.com>
2019-02-20 17:11:12 +0100

    [NEWS] Recursively display comments

--- app_4_news/lib/src/widgets/comment.dart
+++ app_4_news/lib/src/widgets/comment.dart
@@ -18,7 +18,18 @@ class Comment extends StatelessWidget {
           return Text('Still loading comment');
         }
 
-        return Text(snapshot.data.text);
+        final children = <Widget>[
+          Text(snapshot.data.text),
+        ];
+        snapshot.data.kids.forEach((kidId) {
+          children.add(Comment(
+            itemId: kidId,
+            itemMap: itemMap,
+          ));
+        });
+        return Column(
+          children: children,
+        );
       },
     );
   }

3dfaa437367a97e9e5cf734560a271f48bac10f2
lesiak <lesiak@outlook.com>
2019-02-20 16:53:43 +0100

    [NEWS] Changed details widget to a scrollable list

--- app_4_news/lib/src/screens/news_detail.dart
+++ app_4_news/lib/src/screens/news_detail.dart
@@ -39,9 +39,17 @@ class NewsDetail extends StatelessWidget {
                 return Text('Loading');
               }
 
-              return buildTitle(itemSnapshot.data);
+              return buildList(itemSnapshot.data, snapshot.data);
             });
       },
+    );
+  }
+
+  Widget buildList(ItemModel item, Map<int, Future<ItemModel>> itemMap) {
+    return ListView(
+      children: <Widget>[
+        buildTitle(item),
+      ],
     );
   }
 

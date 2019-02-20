254d595a237d63161c1a733e63bbff5d9b7dc1be
lesiak <lesiak@outlook.com>
2019-02-20 13:16:04 +0100

    [NEWS] Show story title on details page

--- app_4_news/lib/src/screens/news_detail.dart
+++ app_4_news/lib/src/screens/news_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -7,11 +10,38 @@ class NewsDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (BuildContext context,
+          AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (BuildContext context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading');
+            }
+
+            return Text(itemSnapshot.data.title);
+          }
+        );
+      },
     );
   }
 }
